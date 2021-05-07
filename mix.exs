9e9126edfc654a8b3db862b1d31d05006dbda566
Hugo <hugolacerdar@gmail.com>
2021-05-07 14:23:10 -0300

    Adding credo config

--- mix.exs
+++ mix.exs
@@ -23,7 +23,8 @@ defmodule Linkly.MixProject do
   defp deps do
     [
       {:postgrex, "~> 0.15"},
-      {:ecto_sql, "~> 3.1"}
+      {:ecto_sql, "~> 3.1"},
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
